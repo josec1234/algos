ec978c21945ee8f49f12f373f65b402cde7d6e73
Jose Carlos <josecralosm@gmail.com>
2023-06-29 19:50:17 +0200

    Algoritmo de ordenación + otros cambios.

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 
 const minValue = 0;
 const maxValue = 10;
+let randomArray;
 const arrayContainer = document.getElementById("arrayContainer");
+document.getElementById("create_btn").addEventListener("click", () => { randomArray = createRandomArray(); });
+document.getElementById("sort_btn").addEventListener("click", sortArray);
 
-document.getElementById("form_arraySize").addEventListener("submit", function() {
-  event.preventDefault();
-  
-
-  // Random array de arraySize
-  const arraySize = document.getElementById("input_arraySize").value;
- 
+function createRandomArray() {
+  event.preventDefault(); // Random array de arraySize
+  const arraySize = document.getElementById("size_input").value;
   let randomArray = [];
   let ol = document.createElement("ol");
   for (let i = 0; i < arraySize; i++) {
@@ -22,6 +21,24 @@ document.getElementById("form_arraySize").addEventListener("submit", function() 
     ol.appendChild(li);
   }
   arrayContainer.appendChild(ol);
-})
+  return randomArray;
+}
 
+
+function sortArray() {
+  console.log("SORTARRAY!!!!!!!!");
+  console.log(randomArray);
+  // Bubble sort
+  console.log(randomArray);
+  for (let i = 0; i < maxValue; i++) {
+    for (let j = 0; j < maxValue -i -1; j++) {
+      if (randomArray[j] > randomArray[j + 1]) {
+        let aux = randomArray[j];
+        randomArray[j] = randomArray[j+1];
+        randomArray[j+1] = aux; 
+      }
+    }
+  }
+  console.log(randomArray);
+}
 
